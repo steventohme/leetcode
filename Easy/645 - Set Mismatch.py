ac8759ca1bfbaeea8c47da0ca03162f400c7b637
Steven Tohme <steventohme59@gmail.com>
2024-01-29 13:55:22 +0000

    Jan 29, 2024, 8:55 AM

--- Easy/645 - Set Mismatch.py
+++ Easy/645 - Set Mismatch.py
@@ -1,9 +1,6 @@
 def findErrorNums(self, nums: list[int]) -> list[int]:
     n = len(nums)
     expectedSum = (n * (n + 1)) / 2
-    seen = set()
+    seen = set(nums)
     arraySum = sum(nums)
-
-    for i in nums:
-        seen.add(i)
         
