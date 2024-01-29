002ea7db775ddc47a3965856003f485c3ab40ebb
Steven Tohme <steventohme59@gmail.com>
2024-01-29 02:38:47 +0000

    Jan 28, 2024, 9:38 PM

--- Medium/2 - Add Two Numbers.py
+++ Medium/2 - Add Two Numbers.py
@@ -3,7 +3,7 @@ class ListNode:
         self.val = val
         self.next = next
 
-def addTwoNumbers( l1: ListNode, l2: ListNode) -> ListNode:
+def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
     dummy = ListNode()
     curr = dummy
 
