dccba9898d27f7525fe6bde4f8600a228ba1c5e0
Steven Tohme <steventohme59@gmail.com>
2024-01-29 19:19:01 +0000

    Jan 29, 2024, 2:19 PM

--- Easy/1 - Two Sum.py
+++ Easy/1 - Two Sum.py
@@ -1,11 +1,11 @@
-# Naive Solution O(n^2) 
+# Naive Solution O(n^2) time complexity, O(1) space complexity
 def twoSumNaive(nums: list[int], target: int) -> list[int]:
     for i in range(len(nums)):
         for j in range(len(nums)):
             if i != j and nums[i] + nums[j] == target:
                 return [i, j]
 
-# Optimal Solution
+# Optimal Solution O(n) time complexity, O(n) space complexity
 def twoSum(nums: list[int], target: int) -> list[int]:
     compliments = {}
 
