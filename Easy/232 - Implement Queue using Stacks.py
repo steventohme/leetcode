fa7c18dbced55b898be62230ef97594a14905a72
Steven Tohme <steventohme59@gmail.com>
2024-01-29 13:43:59 +0000

    Jan 29, 2024, 8:43 AM

--- Easy/232 - Implement Queue using Stacks.py
+++ Easy/232 - Implement Queue using Stacks.py
@@ -12,7 +12,7 @@ class MyQueue:
     def peek(self) -> int:
         if not self.out_stack:
             while self.in_stack:
-                self.push()
+                self.out_sta
 
     def empty(self) -> bool:
         return len(self.in_stack) == 0 and len(self.out_stack) == 0
