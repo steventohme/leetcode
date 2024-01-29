49a35471fb1fd9b0fd7083b2a8d4ec6a64b01b71
Steven Tohme <steventohme59@gmail.com>
2024-01-29 19:13:31 +0000

    Jan 29, 2024, 2:13 PM

---
+++ Easy/118 - Pascal's Triangle.py
@@ -0,0 +1,16 @@
+def generate(numRows: int) -> list[list[int]]:
+    if numRows == 0:
+        return []
+
+    if numRows == 1:
+        return [[1]]
+    
+    prevRows = generate(numRows - 1)
+    newRow = [1] * numRows
+
+    for i in range(1, numRows - 1):
+        newRow[i] = prevRows[-1][i - 1] + prevRows[-1][i]
+
+    prevRows.append(newRow)
+
+    return prevRows
