e54081909684d5769746851b601605a2ffc56ac2
Steven Tohme <steventohme59@gmail.com>
2024-01-29 02:27:24 +0000

    Jan 28, 2024, 9:27 PM

--- Easy/1 - Two Sum.py
+++ Easy/1 - Two Sum.py
@@ -1,7 +1,10 @@
 # Naive Solution
 
-def twoSumNaive(nums, targe):
-    pass
+def twoSumNaive(nums, target):
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i != j and nums[i] + nums[j] == target:
+                return [i, j]
 
 def twoSum(self, nums: List[int], target: int) -> List[int]:
     pass
