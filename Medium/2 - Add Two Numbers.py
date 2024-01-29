b3770807297317e821f48c087476216bb3e8cfd8
Steven Tohme <steventohme59@gmail.com>
2024-01-29 02:36:47 +0000

    Jan 28, 2024, 9:36 PM

--- Medium/2 - Add Two Numbers.py
+++ Medium/2 - Add Two Numbers.py
@@ -10,7 +10,7 @@ def addTwoNumbers( l1: ListNode, l2: ListNode) -> ListNode:
     carry = 0
 
     while l1 or l2 or carry:
+        
         v1 = l1.val if l1 else 0
         v2 = l2.val if l2 else 0
 
-        
