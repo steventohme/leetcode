37aca3249365ff0ac6122938c0c93be14c606ebe
Steven Tohme <steventohme59@gmail.com>
2024-01-29 19:17:25 +0000

    Jan 29, 2024, 2:17 PM

--- Easy/389 - Find the Difference.py
+++ Easy/389 - Find the Difference.py
@@ -5,7 +5,10 @@ def findTheDifference(s: str, t: str) -> str:
         chars_s[i] += 1
     
     chars_t = defaultdict(int)
-    for i in t:
-        chars_t[i] += 1
+    for j in t:
+        chars_t[j] += 1
     
-    
+    for k in chars_t:
+        if chars_s[k] != chars_t[k]:
+            return k
+        
