58f41ad70e40ff35d72fd973681dc5c783cca645
Steven Tohme <steventohme59@gmail.com>
2024-02-02 18:31:26 +0000

    Feb 2, 2024, 1:31 PM

--- Medium/1291 - Sequential Digits.py
+++ Medium/1291 - Sequential Digits.py
@@ -3,4 +3,10 @@ def sequentialDigits(self, low: int, high: int) -> list[int]:
     res = []
 
     start = 0
-    
+    size = 1
+
+    while size < 9:
+        
+        if start == 9:
+            size += 1
+            start = 0
