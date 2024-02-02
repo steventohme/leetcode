7996f7de36f27e9188603097bf02d10fcef48970
Steven Tohme <steventohme59@gmail.com>
2024-02-02 18:23:56 +0000

    Feb 2, 2024, 1:23 PM

--- Medium/1291 - Sequential Digits.py
+++ Medium/1291 - Sequential Digits.py
@@ -1,4 +1,8 @@
 def sequentialDigits(self, low: int, high: int) -> list[int]:
     maximum = "123456789"
+    res = []
 
-    l, r = 0,0
+    for l in range(9):
+        for r in range(9):
+            if maximum[l:r] > low and maximum[l:r] < high:
+                res.append(int())
