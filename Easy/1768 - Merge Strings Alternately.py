1256a7684548055288905631dda1c233fc590632
Steven Tohme <steventohme59@gmail.com>
2024-02-05 14:29:59 +0000

    Feb 5, 2024, 9:29 AM

--- Easy/1768 - Merge Strings Alternately.py
+++ Easy/1768 - Merge Strings Alternately.py
@@ -1,2 +1,19 @@
 def mergeAlternately(word1: str, word2: str) -> str:
-    n, m = len(word1)
+    n, m = len(word1), len(word2)
+
+    i = 0
+    res = ""
+    while i < n and i < m:
+        res += word1[i]
+        res += word2[i]
+        i += 1
+
+    while i < n:
+        res += word1[i]
+        i += 1
+    
+    while i < m:
+        res += word2[i]
+        i += 1
+    
+    return res
