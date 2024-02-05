1b057c03ecd0ec0a5d6cf5b8915bf8d91de200a1
Steven Tohme <steventohme59@gmail.com>
2024-02-05 14:45:18 +0000

    Feb 5, 2024, 9:45 AM

--- Easy/605 - Can Place Flowers.py
+++ Easy/605 - Can Place Flowers.py
@@ -6,4 +6,6 @@ def canPlaceFlowers(flowerbed: list[int], n: int) -> bool:
         if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed) -1 or flowerbed[i + 1] == 0):
             flowerbed[i] = 1
             n -=1
-            
+            if n == 0:
+                return False
+    
