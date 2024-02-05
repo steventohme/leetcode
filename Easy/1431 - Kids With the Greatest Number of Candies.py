e7744f774300992eb795373824212cfa0a170b25
Steven Tohme <steventohme59@gmail.com>
2024-02-05 14:40:54 +0000

    Feb 5, 2024, 9:40 AM

--- Easy/1431 - Kids With the Greatest Number of Candies.py
+++ Easy/1431 - Kids With the Greatest Number of Candies.py
@@ -2,4 +2,5 @@ def kidsWithCandies(candies: list[int], extraCandies: int) -> list[bool]:
     res = [True] * len(candies)
     greatest = max(candies)
 
-    for 
+    for i in range(len(candies)):
+        res[i] 
