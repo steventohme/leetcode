d6dfda36ac7796c92795be6a4a8cf5af233061ca
Steven Tohme <steventohme59@gmail.com>
2024-02-05 15:29:30 +0000

    Feb 5, 2024, 10:29 AM

--- Easy/345 - Reverse Vowels of a String.py
+++ Easy/345 - Reverse Vowels of a String.py
@@ -9,4 +9,16 @@ def reverseVowels(s: str) -> str:
 
     l,r = 0, len(string_list) - 1
 
+    while l < r:
+        if isVowel(string_list[l]) and isVowel(string_list[r]):
+            string_list[l], string_list[r] = string_list[r], string_list[l]
+            l += 1
+            r -= 1
+        
+        elif not isVowel(string_list[l]):
+            l += 1
+        
+        elif not isVowel(string_list[r]):
+            r -= 1
     
+    return ''
