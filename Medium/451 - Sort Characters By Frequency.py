9e7e34675fb1a818fd5a4430f959ff4ee42ac1ff
Steven Tohme <steventohme59@gmail.com>
2024-02-07 17:03:47 +0000

    Feb 7, 2024, 12:03 PM

--- Medium/451 - Sort Characters By Frequency.py
+++ Medium/451 - Sort Characters By Frequency.py
@@ -5,4 +5,6 @@ def frequencySort(s: str) -> str:
     for char in s:
         count[char] += 1
     
-    
+    res = []
+    for k,v in sorted(count.items(), key = lambda x: -x[1]):
+        res += [k]
