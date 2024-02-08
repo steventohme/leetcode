5248f64cd197ff060bc0d3066f42dd6ed80abd64
Steven Tohme <steventohme59@gmail.com>
2024-02-08 03:20:13 +0000

    Feb 7, 2024, 10:20 PM

--- Medium/279 - Perfect Squares.py
+++ Medium/279 - Perfect Squares.py
@@ -1,1 +1,22 @@
-def numSquares(self, n: int) -> int:
+def numSquares(n: int) -> int:
+    def solve(n):
+        if n == 0:
+            return 0
+
+        if n < 0:
+            return float("inf")
+        
+        if memo[n] != -1:
+            return memo[n]
+        
+        mini = n
+        i = 1
+        while i * i <=n:
+            mini = min(mini, solve(n - (i*i)))
+            i += 1
+        
+        memo[n] = mini + 1
+        return memo[n]
+    
+    memo = [-1] * (n + 1)
+    return solve(n)
