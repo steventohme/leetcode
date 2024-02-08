e19b31ae7d7437a275b3f8ddfda1f7d4713ab55f
Steven Tohme <steventohme59@gmail.com>
2024-02-08 14:13:42 +0000

    Feb 8, 2024, 9:13 AM

--- Medium/347 - Top K Frequent Elements.py
+++ Medium/347 - Top K Frequent Elements.py
@@ -1,14 +1,14 @@
 def topKFrequent(nums: list[int], k: int) -> list[int]:
-        frequency_dict = {}
-        for i in nums:
-            if i in frequency_dict:
-                frequency_dict[i] += 1
-            else:
-                frequency_dict[i] = 1
-        
-        sortedList = sorted(frequency_dict.items(), key=lambda item: item[1], reverse=True)
-        res = sortedList[:k]
-        return_list = []
-        for i in res:
-            return_list.append(i[0])
-        return return_list
+    frequency_dict = {}
+    for i in nums:
+        if i in frequency_dict:
+            frequency_dict[i] += 1
+        else:
+            frequency_dict[i] = 1
+    
+    sortedList = sorted(frequency_dict.items(), key=lambda item: item[1], reverse=True)
+    res = sortedList[:k]
+    return_list = []
+    for i in res:
+        return_list.append(i[0])
+    return return_list
