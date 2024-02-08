e0db7a1f1e63166a97d82efd4eb3221edbcf15ea
Steven Tohme <steventohme59@gmail.com>
2024-02-08 14:07:03 +0000

    Feb 8, 2024, 9:07 AM

---
+++ Medium/50 - Pow(x, n).py
@@ -0,0 +1,9 @@
+def myPow(x: float, n: int) -> float:
+    if n == 0:
+        return 1
+    elif n == 1:
+        return x
+    elif n == -1:
+        return 1/x
+    # x^10 = x^5 * x^5 * x^0
+    return myPow(x, n//2) * myPow(x, n//2) * myPow(x, n%2)
