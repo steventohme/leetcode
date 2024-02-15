d33f3f5d2264b123eaaafcf177b60dca032263ca
Steven Tohme <steventohme59@gmail.com>
2024-02-15 06:28:06 +0000

    Feb 15, 2024, 1:28 AM

---
+++ Medium/2971 - Find Polygon With the Largest Perimeter.py
@@ -0,0 +1,11 @@
+    def largestPerimeter(nums: list[int]) -> int:
+        nums.sort()
+        sum_val = 0
+        ans = -1
+        
+        for num in nums:
+            if num < sum_val:
+                ans = num + sum_val
+            sum_val += num
+            
+        return ans
