fcfdb8c7e0e8e3bd6531a3b99fe3ab281b9fb7ec
Steven Tohme <steventohme59@gmail.com>
2024-03-01 00:28:44 +0000

    Feb 29, 2024, 7:28 PM

---
+++ Easy/20 - Valid Parentheses.py
@@ -0,0 +1,16 @@
+def isValid( s: str) -> bool:
+    stack = []
+
+    parentheses = {"{": "}", "(": ")", "[": "]"}
+
+    for bracket in s:
+        if bracket in parentheses:
+            stack.append(bracket)
+        elif stack:
+            open_bracket = stack.pop()
+            if parentheses[open_bracket] != bracket:
+                return False
+        else:
+            return False
+    
+    return len(stack) == 0
