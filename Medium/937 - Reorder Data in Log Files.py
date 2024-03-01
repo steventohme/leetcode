f4a40bc294ef70a8b3888926615231d79e926594
Steven Tohme <steventohme59@gmail.com>
2024-03-01 00:22:39 +0000

    Feb 29, 2024, 7:22 PM

---
+++ Medium/937 - Reorder Data in Log Files.py
@@ -0,0 +1,14 @@
+def reorderLogFiles(, logs: List[str]) -> List[str]:
+    letterLogs = []
+
+    digitLogs = []
+
+    for i in logs:
+        if i.split(" ")[1].isdigit():
+            digitLogs.append(i)
+        else:
+            heapq.heappush(letterLogs, (' '.join(i.split(" ")[1:]), i))
+    
+    result = [heapq.heappop(letterLogs)[1] for _ in range(len(letterLogs))]
+    result.extend(digitLogs)
+    return result
