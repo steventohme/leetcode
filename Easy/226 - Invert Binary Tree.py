84c5e85672b44037126c53cff8f20f108393fd77
Steven Tohme <steventohme59@gmail.com>
2024-03-01 00:20:37 +0000

    Feb 29, 2024, 7:20 PM

---
+++ Easy/226 - Invert Binary Tree.py
@@ -0,0 +1,13 @@
+def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        def invert(root):
+            if not root:
+                return 
+            
+            root.right, root.left = root.left, root.right
+            
+            invert(root.right)
+            invert(root.left)
+        
+        invert(root)
+        return root
