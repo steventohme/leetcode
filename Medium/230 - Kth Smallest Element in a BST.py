d7bc834772446b361ef1669ed0f0ce272fbadcf0
Steven Tohme <steventohme59@gmail.com>
2024-03-03 19:15:17 +0000

    Mar 3, 2024, 2:15 PM

---
+++ Medium/230 - Kth Smallest Element in a BST.py
@@ -0,0 +1,12 @@
+def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        res = []
+        def inorder(node):
+            if not node: return
+            inorder(node.left)
+            if len(res) == k:
+               return 
+            res.append(node.val)
+            inorder(node.right)
+        inorder(root)
+        print(res)
+        return res[-1]
