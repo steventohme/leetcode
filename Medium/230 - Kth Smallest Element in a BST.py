086aee3be341b5f47f1d7b035d8990ae0a52a522
Steven Tohme <steventohme59@gmail.com>
2024-03-03 19:15:54 +0000

    Mar 3, 2024, 2:15 PM

--- Medium/230 - Kth Smallest Element in a BST.py
+++ Medium/230 - Kth Smallest Element in a BST.py
@@ -1,4 +1,8 @@
-
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 def kthSmallest(root: Optional[TreeNode], k: int) -> int:
         res = []
